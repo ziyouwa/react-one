0cf2c1fed85a3a2104ebed71da7dd129f105b4da
YangJie <yjsss75@gmail.com>
2017-01-04 17:09:39 +0800

    始终搞不定muidocs的FontIcon用法，用回materialui原生的

---
+++ src/FontIconExample/FontIconExample.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import FontIcon from 'material-ui/FontIcon';
+
+const iconStyles = {
+    marginRight: 24
+};
+
+const FontIconExample = () => (
+    <div>
+        <FontIcon className="material-icons"  style={iconStyles}>flight_takeoff</FontIcon>
+    </div>
+);
+
+export default FontIconExample;
